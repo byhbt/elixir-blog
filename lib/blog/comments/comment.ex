e91989b20ef23defbc4292aa1463b25c8e5e6f50
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-23 14:23:50 +0700

    Init

---
+++ lib/blog/comments/comment.ex
@@ -0,0 +1,23 @@
+defmodule Blog.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Blog.Posts.Post
+
+  schema "comments" do
+    field :content, :string
+    field :name, :string
+
+    # field :post_id, :id
+    belongs_to(:post, Post)
+
+    timestamps()
+  end 
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:name, :content, :post_id])
+    |> validate_required([:name, :content, :post_id])
+  end
+end
